8b582a457c18bd131208cd8ac1a632ef98cd60a2
Siarhei-Sialitski <siarhei.sialitski.ws@gmail.com>
2023-05-07 20:02:49 +0200

    tailwind

--- src/components/movieListPage/index.tsx
+++ src/components/movieListPage/index.tsx
@@ -68,53 +68,53 @@ const MovieListPage: React.FC = () => {
     ? ''
     : 'flex items-center justify-center h-96 bg-header';
   return (
-    <div className='flex flex-col gap-5 p-5' data-testid='movielistpage'>
+    <div className="flex flex-col gap-5 p-5" data-testid="movielistpage">
       <div className={headerClass}>
         {movieId && (
           <button
-            type='submit'
-            className='absolute top-5 right-5 p-2.5 text-sm font-medium text-rose-500'
+            type="submit"
+            className="absolute right-5 top-5 p-2.5 text-sm font-medium text-rose-500"
             onClick={() => {
               navigate({ pathname: `/`, search: searchParams.toString() });
             }}
-            data-testid='searchicon'
+            data-testid="searchicon"
           >
             <svg
-              aria-hidden='true'
-              className='w-5 h-5'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
+              aria-hidden="true"
+              className="h-5 w-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               ></path>
             </svg>
           </button>
         )}
         <Outlet />
-        <div id='portal-root' />
+        <div id="portal-root" />
       </div>
 
-      <div className='px-5'>
-        <div className='flex flex-row justify-between'>
+      <div className="px-5">
+        <div className="flex flex-row justify-between">
           <GenreSelect
             genreNames={genres}
             selectedGenre={searchParams.get('genre') ?? allGenre}
             onSelect={handleGenreSelect}
           />
-          <div className='float-right'>
+          <div className="float-right">
             <SortControl
               currentSelection={searchParams.get('sortBy') ?? sortCriterias[0]}
               onSelectionChanged={handleSortingSelect}
             />
           </div>
         </div>
-        <div className='flex flex-wrap py-3 px-2 gap-x-16 gap-y-8'>{tiles}</div>
+        <div className="flex flex-wrap gap-x-16 gap-y-8 px-2 py-3">{tiles}</div>
       </div>
     </div>
   );
